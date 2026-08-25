22777882c96eb18f8acc72bd293de9c8e109cc32
dulphMain <augustin.michau@outlook.com>
2026-08-25 13:17:19 +0200

    Normalized to W3C prescriptions

--- includes/menus.php
+++ includes/menus.php
@@ -1,6 +1,6 @@
 <div class="col-1">
 <div class="panel panel-default margin-10 pattern-bg">
-	<div class="panel-heading"><h2><img alt="pub" src="images/accueil/pub.png"  style="margin-right: 10px"/>Publicité</h2></div>
+	<div class="panel-heading"><h2><img alt="pub" src="images/accueil/pub.png"  style="margin-right: 10px">Publicité</h2></div>
 	<div class="panel-body">
 	<!--<p style="text-align: center">
 	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
@@ -15,7 +15,7 @@
 	</div>
 	</div>
 	<div class="panel panel-default margin-10 pattern-bg">
-	<div class="panel-heading"><h2><img alt="news" src="images/accueil/news.png"  style="margin-right: 10px"/>News</h2></div>
+	<div class="panel-heading"><h2><img alt="news" src="images/accueil/news.png"  style="margin-right: 10px">News</h2></div>
 	<div class="panel-body">
 	<?php
 	$retour = mysqli_query($base,'SELECT * FROM news ORDER BY id DESC LIMIT 0, 1');
@@ -29,7 +29,7 @@
 		?>
 		<span class="important"><?php echo $donnees['titre']; ?><em> le <?php echo date('d/m/Y Ã H\hi', $donnees['timestamp']); ?></em></span>  
 		<p>
-		<br/>
+		<br>
 		<?php
 		$contenu = nl2br(stripslashes($donnees['contenu']));
 		echo $contenu;
@@ -40,21 +40,21 @@
 	</div>
 	
 	<div class="panel panel-default margin-10 pattern-bg">
-	<div class="panel-heading"><h2><img alt="stats" src="images/accueil/statistiques.png"  style="margin-right: 10px"/>Statistiques</h2></div>
+	<div class="panel-heading"><h2><img alt="stats" src="images/accueil/statistiques.png"  style="margin-right: 10px">Statistiques</h2></div>
 	<div class="panel-body">
-	<p><img alt="user" src="images/accueil/user.png" style="margin-right: 5px"/>Inscrits :
+	<p><img alt="user" src="images/accueil/user.png" style="margin-right: 5px">Inscrits :
 	<?php $sql = 'SELECT count(*) AS c FROM membre';
-	$req = mysqli_query($base,$sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
+	$req = mysqli_query($base,$sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error());
 	$inscrits = mysqli_fetch_array($req);
 	echo $inscrits['c'];?>
 	</p>
-	<p><img alt="actifs" src="images/accueil/actifs.png" style="margin-right: 5px"/>Actifs :
+	<p><img alt="actifs" src="images/accueil/actifs.png" style="margin-right: 5px">Actifs :
 	<?php
 	$req = mysqli_query($base,'SELECT count(*) AS c FROM membre WHERE derniereConnexion >=\''.(time()-2678400).'\'');
 	$actifs = mysqli_fetch_array($req);
 	echo $actifs['c'];?>
 	</p>
-	<p><img alt="connectes" src="images/accueil/connectes.png" style="margin-right: 5px"/><a href="connectes.php">Connectés</a> : <?php $retour = mysqli_query($base,'SELECT COUNT(*) AS nbre_entrees FROM connectes');
+	<p><img alt="connectes" src="images/accueil/connectes.png" style="margin-right: 5px"><a href="connectes.php">Connectés</a> : <?php $retour = mysqli_query($base,'SELECT COUNT(*) AS nbre_entrees FROM connectes');
 	$connectes = mysqli_fetch_array($retour); 
 	echo $connectes['nbre_entrees']; ?>
 	</p> 
@@ -62,12 +62,12 @@
 	</div>
 	
 	<div class="panel panel-default margin-10 pattern-bg">
-	<div class="panel-heading"><h2><img alt="screen" src="images/accueil/screen.png"  style="margin-right: 10px"/>Screenshots</h2></div>
+	<div class="panel-heading"><h2><img alt="screen" src="images/accueil/screen.png"  style="margin-right: 10px">Screenshots</h2></div>
 	<div class="panel-body">
-	<a href="images/screenshoots/screen1.png" rel="lightbox[screenshoots]"><img alt="screenMini1" src="images/screenshoots/screen1.png" class="img-square img-thumbnail" style="width:200;height:113;"/></a>
-	<a href="images/screenshoots/screen2.png" rel="lightbox[screenshoots]"><img alt="screenMini2" src="images/screenshoots/screen2.png" class="img-square img-thumbnail" style="width:200;height:113;"/></a>
-	<a href="images/screenshoots/screen3.png" rel="lightbox[screenshoots]"><img alt="screenMini3" src="images/screenshoots/screen3.png" class="img-square img-thumbnail" style="width:200;height:113;"/></a>
-	<a href="images/screenshoots/screen4.png" rel="lightbox[screenshoots]"><img alt="screenMini3" src="images/screenshoots/screen4.png" class="img-square img-thumbnail" style="width:200;height:113;"/></a>
+	<a href="images/screenshoots/screen1.png" rel="lightbox[screenshoots]"><img alt="screenMini1" src="images/screenshoots/screen1.png" class="img-square img-thumbnail" style="width:200;height:113;"></a>
+	<a href="images/screenshoots/screen2.png" rel="lightbox[screenshoots]"><img alt="screenMini2" src="images/screenshoots/screen2.png" class="img-square img-thumbnail" style="width:200;height:113;"></a>
+	<a href="images/screenshoots/screen3.png" rel="lightbox[screenshoots]"><img alt="screenMini3" src="images/screenshoots/screen3.png" class="img-square img-thumbnail" style="width:200;height:113;"></a>
+	<a href="images/screenshoots/screen4.png" rel="lightbox[screenshoots]"><img alt="screenMini3" src="images/screenshoots/screen4.png" class="img-square img-thumbnail" style="width:200;height:113;"></a>
 	</div>
 	</div>
 	</div>
