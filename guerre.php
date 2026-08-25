52f44e69ed8026f89010ece5e57a49a171d5c1da
dulphMain <augustin.michau@outlook.com>
2026-08-25 13:52:58 +0200

    Normalized to W3C prescriptions

--- guerre.php
+++ guerre.php
@@ -27,27 +27,27 @@ if(isset($_GET['id'])) {
         debutCarte('<a href="alliance.php?id='.$alliance1['tag'].'" style="color:white"><span class="lienTitre">'.$alliance1['tag'].'</span></a> VS <a href="alliance.php?id='.$alliance2['tag'].'"><span class="lienTitre" style="color:white">'.$alliance2['tag'].'</span></a>');
 		echo '
 		<p>
-		<span class="subimportant">Nombre de pertes totales : </span>'.number_format(($guerre['pertes1'] + $guerre['pertes2']), 0 , ' ', ' ').' molécules dont<br/>';
+		<span class="subimportant">Nombre de pertes totales : </span>'.number_format(($guerre['pertes1'] + $guerre['pertes2']), 0 , ' ', ' ').' molécules dont<br>';
 		if($guerre['pertes1'] + $guerre['pertes2'] > 0) {
 			echo '
-			'.number_format($guerre['pertes1'], 0 , ' ', ' ').' molécules pour <a href="alliance.php?id='.$alliance1['tag'].'">'.$alliance1['tag'].'</a> ('.round($guerre['pertes1']/($guerre['pertes1'] + $guerre['pertes2'])*100).'%)<br/>
-			'.number_format($guerre['pertes2'], 0 , ' ', ' ').' molécules pour <a href="alliance.php?id='.$alliance2['tag'].'">'.$alliance2['tag'].'</a> ('.round($guerre['pertes2']/($guerre['pertes1'] + $guerre['pertes2'])*100).'%)<br/>
+			'.number_format($guerre['pertes1'], 0 , ' ', ' ').' molécules pour <a href="alliance.php?id='.$alliance1['tag'].'">'.$alliance1['tag'].'</a> ('.round($guerre['pertes1']/($guerre['pertes1'] + $guerre['pertes2'])*100).'%)<br>
+			'.number_format($guerre['pertes2'], 0 , ' ', ' ').' molécules pour <a href="alliance.php?id='.$alliance2['tag'].'">'.$alliance2['tag'].'</a> ('.round($guerre['pertes2']/($guerre['pertes1'] + $guerre['pertes2'])*100).'%)<br>
 			';
 		}
 		else {
 			echo '
-			0 molécules pour <a href="alliance.php?id='.$alliance1['tag'].'">'.$alliance1['tag'].'</a> (0%)<br/>
-			0 molécules pour <a href="alliance.php?id='.$alliance2['tag'].'">'.$alliance2['tag'].'</a> (0%)<br/>
+			0 molécules pour <a href="alliance.php?id='.$alliance1['tag'].'">'.$alliance1['tag'].'</a> (0%)<br>
+			0 molécules pour <a href="alliance.php?id='.$alliance2['tag'].'">'.$alliance2['tag'].'</a> (0%)<br>
 			';
 		}
-		echo '<br/><span class="subimportant">Date de début de la guerre : </span>'.date('d/m/Y à H\hi', $guerre['timestamp']).'<br/>';
+		echo '<br><span class="subimportant">Date de début de la guerre : </span>'.date('d/m/Y à H\hi', $guerre['timestamp']).'<br>';
 		
 		if($guerre['fin'] > $guerre['timestamp']) {
-			echo '<span class="subimportant">Date de fin de la guerre : </span>'.date('d/m/Y à H\hi', $guerre['fin']).'<br/>
+			echo '<span class="subimportant">Date de fin de la guerre : </span>'.date('d/m/Y à H\hi', $guerre['fin']).'<br>
 			Cette guerre a donc duré '.round(($guerre['fin'] - $guerre['timestamp'])/86400).' jours.';
 		}
 		else {
-			echo '<span class="subimportant">Date de fin de la guerre : </span>Non finie<br/>';
+			echo '<span class="subimportant">Date de fin de la guerre : </span>Non finie<br>';
 		}
 		echo '</p>';
         finCarte();
