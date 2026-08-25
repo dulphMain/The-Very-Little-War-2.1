37a3521662aa9d728c6c23f9580fb4c7b7d3329a
dulphMain <augustin.michau@outlook.com>
2026-08-25 13:54:38 +0200

    Normalized to W3C prescriptions

--- credits.php
+++ credits.php
@@ -15,7 +15,7 @@ debutCarte("Crédits");
 debutContent();
 echo important("Images");
 ?>
-Ce site regorge de nombreuses images sous license copyright provenant du site web <a href="https://www.flaticon.com/" class="lienVisible">FlatIcon</a>. Voici la liste des auteurs des différentes images : Freepik, VectorMarket, RoundIcons, Madebyoliver, Nikita Golubev, Pixel Buddha, Yannick, Creaticca Creative Agency, Dave Gandy, Dimitry, Gregor Cresnar, Alessio Atzeni...<br/><br/>
+Ce site regorge de nombreuses images sous license copyright provenant du site web <a href="https://www.flaticon.com/" class="lienVisible">FlatIcon</a>. Voici la liste des auteurs des différentes images : Freepik, VectorMarket, RoundIcons, Madebyoliver, Nikita Golubev, Pixel Buddha, Yannick, Creaticca Creative Agency, Dave Gandy, Dimitry, Gregor Cresnar, Alessio Atzeni...<br><br>
 Si vous trouvez des images vous appartenant sur ce site et que vous n'êtes pas crédité, veuillez me faire part de votre demande par l'intérmédiaire de <strong>Contact</strong>.
 
 <?php
