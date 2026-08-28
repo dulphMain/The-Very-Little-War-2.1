da7dc671883faa1436acf6571f6f9bc48f753a18
dulphMain <augustin.michau@outlook.com>
2026-08-28 13:29:35 +0200

    Corrected http links to https

--- includes/partenariat.php
+++ includes/partenariat.php
@@ -5,7 +5,7 @@ padding:0;
 outline:0
 }
 </style>
-<script src="http://www.theverylittlewar.com/images/partenariat/jquery.js" type="text/javascript"></script>
-<script type="text/javascript" src="http://www.theverylittlewar.com/images/partenariat/charger_barre.js"></script>
-<script type="application/javascript" src="http://www.theverylittlewar.com/images/partenariat/news.json"></script>
-<link href="http://www.theverylittlewar.com/images/partenariat/style_barre.css" rel="stylesheet" type="text/css">
+<script src="https://www.theverylittlewar.com/images/partenariat/jquery.js" type="text/javascript"></script>
+<script type="text/javascript" src="https://www.theverylittlewar.com/images/partenariat/charger_barre.js"></script>
+<script type="application/javascript" src="https://www.theverylittlewar.com/images/partenariat/news.json"></script>
+<link href="https://www.theverylittlewar.com/images/partenariat/style_barre.css" rel="stylesheet" type="text/css">
